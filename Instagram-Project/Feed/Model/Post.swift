62014d4fdc4b748e3fa15d7ee191f94f22e11ad6
Alexander <alexsahnykov@gmail.com>
2018-08-19 19:05:02 +0300

    prepare for feed/comments work

---
+++ Instagram-Project/Feed/Model/Post.swift
@@ -0,0 +1,9 @@
+//
+//  Post.swift
+//  Instagram-Project
+//
+//  Created by Александр Сахнюков on 19/08/2018.
+//  Copyright © 2018 Александр Сахнюков. All rights reserved.
+//
+
+import Foundation
